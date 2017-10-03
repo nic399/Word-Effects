a3d778208bff317d4029c92e3a0e33d51afba1a0
Nicholas Fung <nbfung@sfu.ca>
2017-10-02 17:55:53 -0700

    Initial Commit

---
+++ Word-Effects/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Word-Effects
+//
+//  Created by Nicholas Fung on 2017-10-02.
+//  Copyright © 2017 Nicholas Fung. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
